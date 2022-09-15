152020d1dd7384d86b88868f903dbca90295bb35
Alvar <alvarbu@gmail.com>
2022-09-15 18:30:12 -0500

    se agrega favicon

--- resources/views/layouts/hapix-layout.blade.php
+++ resources/views/layouts/hapix-layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Splits</title>
+    <title>¡Agilizamos la cobranza de tu negocio! - Hapix</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
         integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.css">
